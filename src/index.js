de66926b08a7e6fd861cfe0f9d0a1da69309f3da
cyph404 <nicoleblanchette05@gmail.com>
2023-10-21 18:37:55 -0400

    Completed MVP stage

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const welcomeAndGetName = () => {
   return name.trim();
 };
 
+const getUserNumber = () => {
+  const chosenNum = Number(prompt('Take your pick: ').trim())
+  return chosenNum
+};
+
 const showOptions = () => {
   console.log('Here are your options:');
   console.log('1 - play a guessing game');
@@ -17,9 +22,46 @@ const showOptions = () => {
 
 const sayGoodbye = (name) => console.log(`Goodbye, ${name}`);
 
+const getRandomIntInRange = (min, max) => (Math.random() * (max - min)) + min;
+
+const checkAnswer = (guess, answer) => {
+  if (guess === answer) return 'Congratulations, you won!'
+  return 'Better luck next time!'
+};
+
+const handleGuessingGame = () => {
+  const guess = Number(prompt('Guess a number from 1-10! ').trim())
+  const answer = getRandomIntInRange(0, 11)
+  const result = checkAnswer(guess, answer)
+  console.log(result)
+  return result
+}
+
+const handleWordsOfWisdom = () => {
+}
+
+const handleCheerYouOn = () => {
+}
+
+const handleSubmit = () => {
+  const option = getUserNumber();
+  switch (option) {
+    case 1:
+      handleGuessingGame();
+      break;
+    case 2:
+      handleWordsOfWisdom();
+      break;
+    case 3:
+      handleCheerYouOn();
+      break;
+  }
+};
+
 const main = () => {
   const name = welcomeAndGetName();
   const options = showOptions();
+  const submission = handleSubmit();
   const goodbye = sayGoodbye(name);
 };
 
